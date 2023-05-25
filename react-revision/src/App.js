84a39bb14978aa93caeb64a71a0ff4346b088e0d
Harshda Mhase <harshadamhase12@gmail.com>
2023-05-24 16:02:51 -0900

    doc doumentaion

--- react-revision/src/App.js
+++ react-revision/src/App.js
@@ -1,14 +1,26 @@
-
 import './App.css';
 
-
-
+import React, { useState } from 'react';
 
 function App() {
+  const [name,setName] = useState("");
+  const [college,setCollege] = useState("");
   return (
-    <div >
-      
-    </div>
+    <>
+      <form>
+        <input type='text' value={name}onChange={(e)=>{
+          setName(e.target.value);
+        }
+      }
+          /><br/>
+           <input type='text' value={college}onChange={(e)=>{
+          setCollege(e.target.value);
+        }
+      }
+          />
+        <h1>{name} 'is studyng </h1>
+      </form>
+    </>
   );
 }
 
